33665f3bd352fb4091fd321ec7a4eb77e3d9fb0e
wrrngton <hello@jezl.xyz>
2025-09-09 18:58:20 +0100

    dates

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,28 +1,48 @@
 import syntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
+import util from "util";
 
-export default async function (eleventyConfig) {
+export default async function(eleventyConfig) {
   eleventyConfig.setInputDirectory("input");
+
+  eleventyConfig.addFilter("dump", (obj) => {
+    return util.inspect(obj);
+  });
 
   eleventyConfig.addPlugin(syntaxHighlight);
 
-  eleventyConfig.addCollection("posts", function (collectionApi) {
+  eleventyConfig.addCollection("posts", function(collectionApi) {
     return collectionApi.getFilteredByGlob("input/posts/*.md");
   });
 
-  eleventyConfig.addCollection("snippets", function (collectionApi) {
+  eleventyConfig.addCollection("snippets", function(collectionApi) {
     return collectionApi.getFilteredByGlob("input/snippets/*.md");
   });
 
   eleventyConfig.addCollection("recentPosts", function(collectionApi) {
-    return collectionApi.getFilteredByGlob("input/posts/*.md").reverse().slice(0,2);
+    return collectionApi
+      .getFilteredByGlob("input/posts/*.md")
+      .reverse()
+      .slice(0, 2);
   });
 
   eleventyConfig.addFilter("getDate", function(dateStr, format) {
     const date = new Date(dateStr);
     if (format === "year") {
       return date.getFullYear();
-    }
-    else return;
+    } else return;
+  });
+
+  eleventyConfig.addFilter("postDate", function(dateStr) {
+    const date = new Date(dateStr);
+    const options = {
+      year: "numeric",
+      month: "numeric",
+      day: "numeric",
+    };
+
+    const newDateStr = date.toLocaleDateString("en-GB", options);
+    const finalDateStr = newDateStr.replaceAll("/", "-");
+    return finalDateStr;
   });
 
   eleventyConfig.addFilter("alphSort", function(suppliedArr) {
